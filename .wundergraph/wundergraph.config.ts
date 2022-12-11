6e58e9df32e2a3227ec8f7059ea0887e0ec70740
Abdo Alsayed <abdelhameed.h@gmail.com>
2022-12-11 13:13:37 +0200

    env vars db

--- .wundergraph/wundergraph.config.ts
+++ .wundergraph/wundergraph.config.ts
@@ -15,7 +15,7 @@ const spacex = introspect.graphql({
 
 const db = introspect.postgresql({
   apiNamespace: 'db',
-  databaseURL: process.env.DATABASE_URL.toString(),
+  databaseURL: new EnvironmentVariable('DATABASE_URL'),
 })
 
 // configureWunderGraph emits the configuration
