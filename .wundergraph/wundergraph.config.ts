a80414dfae302abb17eb5ac218e3043918672c57
Abdo Alsayed <abdelhameed.h@gmail.com>
2022-12-11 11:46:16 +0200

    adding schema

--- .wundergraph/wundergraph.config.ts
+++ .wundergraph/wundergraph.config.ts
@@ -15,7 +15,7 @@ const spacex = introspect.graphql({
 
 const db = introspect.postgresql({
   apiNamespace: 'db',
-  databaseURL: 'postgresql://postgres:hamido@411_@db.iznphlblnswbbpecznsq.supabase.co:6543/postgres',
+  databaseURL: 'postgresql://postgres:hamido@411_@db.iznphlblnswbbpecznsq.supabase.co:6543/postgre?schema=public',
 })
 
 // configureWunderGraph emits the configuration
